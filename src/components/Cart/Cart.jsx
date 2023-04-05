df5b0b84d911fefc98faffee21c7d36d3d82e50d
unknown <you@example.com>
2023-04-05 19:32:50 +0600

    added clear cart btn

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import './Cart.css'
 
 const Cart = ({cart}) => {
@@ -24,6 +26,10 @@ const Cart = ({cart}) => {
                 <p>Shipping :${totalShipping}</p>
                 <p>Tax :${tax.toFixed(2)}</p>
                 <h6>Grand Total :${grandTotal.toFixed(2)}</h6>
+                <button  className='clear-cart-btn'>
+                    <span>Clear Cart</span>
+                    <FontAwesomeIcon icon={faTrashCan} />
+                </button>
         </div>
     );
 };
